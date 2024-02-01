19ecc14e65d4ad21fd73a044917a0dd3d8020a4f
DavidCaba6194 <cabayosoyasi@gmail.com>
2024-02-01 19:27:59 +0100

    Chat mejorado

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="chat-container">
+  <div class="chat-container" ref="chat">
+    <div class="chat-header">
+      <input class="input-channel" type="text" placeholder="ID del canal" v-model="channelID">
+      <div class="btn-connect" @click="connectChat()">Connect</div>
+      <div class="btn-disconnect" @click="disconnectChat()">Disconnect</div>
+    </div>
     <div v-for="(msg, i) in chatHistory" :key="i">
       <div class="msg-box">
         <div class="time">{{msg.time.getHours() < 10 ? '0' + msg.time.getHours() : msg.time.getHours()}}:{{msg.time.getMinutes() < 10 ? '0' + msg.time.getMinutes() : msg.time.getMinutes()}}</div>
@@ -17,24 +22,36 @@ export default {
   name: 'ChatBox',
   data() {
     return {
-      chatHistory: []
+      channelID: '',
+      chatHistory: [],
+      client: null
     }
   },
   components: {
     
   },
   created() {
-    this.loadChat();
+    
   },
   methods: {
+    connectChat() {
+      if (this.channelID !== '') {
+        this.loadChat();
+      }
+    },
+    disconnectChat() {
+      if (this.client) {
+        this.client.disconnect();
+      }
+    },
     loadChat() {
-      const client = new tmi.Client({
-        channels: [ 'IlloJuan' ]
+      this.client = new tmi.Client({
+        channels: [ this.channelID ]
       });
 
-      client.connect();
+      this.client.connect();
 
-      client.on('message', (channel, tags, message, self) => {
+      this.client.on('message', (channel, tags, message, self) => {
         // console.log(`${channel}: ${self}`);
         // console.log(tags);
         let obj = {
@@ -48,7 +65,13 @@ export default {
           time: new Date()
         };
         this.chatHistory.push(obj);
+        this.scrollToBottom();
       });
+    },
+    scrollToBottom() {
+      if (this.chatHistory.length > 0) {
+        this.$refs.chat?.scrollTo(0, this.$refs.chat.scrollHeight);
+      }
     }
   }
 }
@@ -57,14 +80,73 @@ export default {
 .chat-container {
   position: fixed;
   width: 350px;
-  height: calc(100vh - 50px);
+  height: calc(100vh - 90px);
   top: 50px;
   right: 0px;
   background-color: #282b30;
-  padding: 10px;
+  padding: 20px 10px;
+  padding-top: 60px;
   overflow: auto;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
   z-index: 8;
+}
+
+.chat-header {
+  position: fixed;
+  width: 350px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  padding: 10px;
+  top: 50px;
+  right: 0px;
+  background-color: #282b30;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+.input-channel {
+  border: 0px;
+  border-bottom: 1px solid #707377;
+  color: #b4b4b4;
+  background-color: #282b30;
+  padding: 5px;
+  outline: none;
+}
+
+.btn-connect {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 70px;
+  height: 25px;
+  border-radius: 5px;
+  background-color: #bb7cfd;
+  color: #ffffff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+}
+
+.btn-connect:hover {
+  background-color: #9d41ff;
+}
+
+.btn-disconnect {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90px;
+  height: 25px;
+  border-radius: 5px;
+  background-color: #fd7c7c;
+  color: #ffffff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+}
+
+.btn-disconnect:hover {
+  background-color: #ff4a4a;
 }
 
 .msg-box {
@@ -111,5 +193,26 @@ export default {
   font-weight: 300;
   color: #e2e2e2;
   margin-left: 5px;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 7px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #282b30; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #707377;
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #707377; 
 }
 </style>
--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="menu">
+    <div class="game-card">
+      Ahorcado
+    </div>
   </div>
 </template>
 
@@ -18,5 +21,9 @@ export default {
   top: 50px;
   left: 0px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap;
 }
 </style>
